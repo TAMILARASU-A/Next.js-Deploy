45a592b606d721a812b303ceacb4633499422638
TAMILARASU A <127194884+TAMILARASU-A@users.noreply.github.com>
2025-06-11 01:29:49 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          defer
+        ></script>
       </body>
     </Html>
   )
