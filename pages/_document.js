b09d9dee6991cdee291c5998b8b1bece11a119b4
TAMILARASU A <127194884+TAMILARASU-A@users.noreply.github.com>
2025-06-11 01:22:29 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
